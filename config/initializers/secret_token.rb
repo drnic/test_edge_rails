5d1ad599756f9f2dc300ea7a593002cf7e4f5f70
Dr Nic Williams <drnicwilliams@gmail.com>
2011-04-14 10:32:26 -0700

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TestEdgeRails::Application.config.secret_token = 'f9a462ba4c12cf35a5b46e8df9da2d22cfc9cb5b84f20ff0fd236da7d097514fd5c90cc1886914753d5ee0cdefdc555ce6935b7297ff481a52c17876dfb7a31f'
